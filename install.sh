ebf88e6b9c8556afea44fbc5171e0dd0f8ddd1ed
Jason Hughes <jason.hughes@skydio.com>
2026-05-08 15:32:02 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# Coder clones this repo to ~/dotfiles, so $(pwd) when this runs is ~/dotfiles
+sudo apt-get update
+sudo apt-get install -y neovim
+
 mkdir -p "$HOME/.config"
 ln -sfn "$(pwd)" "$HOME/.config/nvim"
+
+nvim --headless "+Lazy! sync" +qa || true
+
